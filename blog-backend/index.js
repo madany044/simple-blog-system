1f7b92fe0357c7a709fa141e123a47a61b124218
bk0173915-source <bk0173915-source-bk0173915@gmail.com>
2026-02-26 10:42:09 +0530

    modified functionality

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -7,7 +7,9 @@ const postRoutes = require("./routes/posts");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
